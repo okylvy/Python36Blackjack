137e86e91e3fd6319af27d94e7d63761a8368fb9
Hiroki Ikenaga <okylvy@yahoo.co.jp>
2019-05-19 21:25:52 +0900

    Add some classes for 1st step of object oriented design.

--- blackjack.py
+++ blackjack.py
@@ -1,8 +1,44 @@
 #!/usr/bin/python3.6
 #-*- coding:utf-8 -*-
 
+class Stock:
+	def __init__(self):
+		pass
+
+class Player:
+	def __init__(self):
+		print("skeleton")
+#	def isContinue():
+#		print("Continue? [yes/no]")
+
+class Dealer:
+	def __init__(self):
+		pass
+
+class GameManager:
+	def __init__(self):
+#		self.player = Player()
+#		self.dealer = Dealer()
+		self.name = "pythonBeginner"
+
+	def judge(self):
+		print("You win.")
+
+def isContinue():
+	print("Continue? [yes/no]")
+	return False
+
 def main():
+	game = GameManager()
+
 	print("===== Game Start ===== ")
+	print("")
+
+	while True:
+		if not isContinue():
+			break
+
+	game.judge()
 
 if __name__ == '__main__':
 	main()
